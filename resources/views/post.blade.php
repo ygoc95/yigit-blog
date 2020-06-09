3962add9df42a456655171d3d65d8571f78e1349
ygoc95 <ygoc95@gmail.com>
2020-06-09 23:15:21 +0200

    sidebar change css

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -3,15 +3,17 @@
     {{$post->title}} - Yigit Goc Blog
 @endsection
 @section('container')
-<h1 class="title">{{$post->title}}</h1>
-<h3>{{$post->created_at->format('d M Y')}}</h3>
-<hr>
-<p>{!! $post->md_summary !!}</p>
-@if (Auth::check())
-    <form name="delete_form" method="POST">
-    @csrf
-    @method('DELETE')
-    <button type="submit" class="btn btn-primary">Delete</button>
-    </form>
+
+    <h1 class="title">{{$post->title}}</h1>
+    <h3>{{$post->created_at->format('d M Y')}}</h3>
+    <hr>
+    <p>{!! $post->md_summary !!}</p>
+    @if (Auth::check())
+        <form name="delete_form" method="POST">
+        @csrf
+        @method('DELETE')
+        <button type="submit" class="btn btn-primary">Delete</button>
+        </form>
+ 
 @endif
 @endsection
